5bb8ca1b85f5186c7500037746f56ac76505efd9
Simon Thelen <sith98@web.de>
2022-06-10 21:12:45 +0200

    Add helper class Buffer

--- Utils.cpp
+++ Utils.cpp
@@ -13,3 +13,30 @@ void dumpInfo(byte* ar, int len) {
 uint32_t parseAppId(byte* buffer) {
     return buffer[0] | buffer[1] << 8 | buffer[2] << 6;
 }
+
+template <size_t N>
+boolean Buffer<N>::append(byte b) {
+    CHECK_SIZE(1)
+    buffer[size] = b;
+    size++;
+    return true;
+}
+template <size_t N>
+boolean Buffer<N>::append24(uint32_t value) {
+    CHECK_SIZE(3)
+    buffer[size] = (value >> 0) & 0xff;
+    buffer[size + 1] = (value >> 8) & 0xff;
+    buffer[size + 2] = (value >> 16) & 0xff;
+    size += 3;
+    return true;
+}
+template <size_t N>
+boolean Buffer<N>::append32(uint32_t value) {
+    CHECK_SIZE(4)
+    buffer[size] = (value >> 0) & 0xff;
+    buffer[size + 1] = (value >> 8) & 0xff;
+    buffer[size + 2] = (value >> 16) & 0xff;
+    buffer[size + 3] = (value >> 24) & 0xff;
+    size += 4;
+    return true;
+}
--- Utils.h
+++ Utils.h
@@ -4,3 +4,16 @@
 void dumpInfo(byte*, int);
 
 uint32_t parseAppId(byte* buffer);
+
+#define CHECK_SIZE(s) \
+    if (size + s - 1 >= N) return false;
+
+template <size_t N>
+class Buffer {
+   public:
+    byte buffer[N];
+    size_t size = 0;
+    boolean append(byte b);
+    boolean append24(uint32_t value);
+    boolean append32(uint32_t value);
+};
